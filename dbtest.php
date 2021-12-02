4470ff1b06671302cf922af9dcb524554bf13036
Harshada Deokar <46487473+HarshadaDeokar317@users.noreply.github.com>
2021-12-02 23:00:49 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -10,7 +10,7 @@ if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 } else {
-    printf("Connected to the database");
+    printf("Hi ,Connected to the database");
 }
 $connection->close();
 ?>
